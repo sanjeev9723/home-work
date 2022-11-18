27dba03a597f32977ea26ec199ac6de030c2fd7b
sanjeev9723 <113090703+sanjeev9723@users.noreply.github.com>
2022-11-19 00:31:39 +0530

    Add files via upload

---
+++ home-work/app.js
@@ -0,0 +1,73 @@
+     
+   
+
+        /*const number = parseInt(prompt("Enter a number : ")); 
+         for (let i = 1; i <= 10; i++) {
+         console.log(number + "*" + i + "=" + number * i);
+       }*/
+       
+       
+    //    function LengthConverter(valNum) {
+    //   console.log("LengthConverter"+"="+valNum*0.62137);
+    //   }
+
+     /* function sumArray(array) {
+        const ourArray = [1, 2, 10, 9, -3];
+        let sum = 0;
+      
+        for (let i = 0; i < ourArray.length; i += 1) {
+          sum += ourArray[i];
+        }
+        
+        return sum;
+      }
+      
+      console.log(sumArray([1, 2, 10, 9, -3]));*/
+
+     /* const array1 = [10, 22, 35, 52, 62];
+      const reversed = array1.reverse();
+      console.log('reversed:', reversed);*/
+
+     /* const points = [40, 100, 1, 5, 25, 10];
+      points.sort(function(a, b){return a-b});
+      numberArray.sort(compareNumbers);*/
+
+      /*const points = [40, 100, 1, -5, -25, 10];
+      for(i=0;i<points.length;i++){
+        if(points[i]>=0){
+            console.log(points[i])
+        }
+      }*/
+     
+     /* var  value= "hello my world";
+      var array=value.split(" ");
+      var result="";
+      for(i=0;i<array.length;i++){
+       result=result+""+array[i];
+       }
+    console.log(result)*/
+
+
+      /* const number = parseInt(prompt("Enter a number : ")); 
+         if(number%10==0) {
+         console.log("true");
+         }
+         else{
+            console.log("false");
+         }*/
+
+    /*var  value= "sanjeevkumar";
+      var vowel=['a','e','i','o','u'];
+      var result=0;
+    
+      for(i=0;i<vowel.length;i++){
+        var count=0;
+      for(j=0;j<value.length;j++){
+    if (value[j]==vowel[i]){
+    count++;
+    result++;
+    }
+      }
+      console.log(vowel[i]+"-"+count)
+       }
+    console.log("totalcount -" +result) */
